13242f732fc3e586b36996cd1826712833b86b29
gracel202 <linlgrace556@gmail.com>
2021-02-10 23:22:03 -0500

    change

--- app.js
+++ app.js
@@ -1,50 +1,39 @@
-//comments in js
-var life = 100; //number
-life = life - 10; //90
-life = life - 10; //80
-console.log(life);
-
-// var name = "Grace"; //string
-// console.log(name);
-
-console.log("hey");
-
-var checkout = true; //boolean
-var checkout = false; //boolean
-
-var box;
-console.log(box); //undefined
-
-var box = null; //no value
-
-// use const for selecting img
-// const life = 100;
-// can't redfine life
-
-// let life = 100;
-// life = life - 50;
-// console.log(life);
-
-const myAge = 18;
-const yourAge = 21;
-console.log(myAge + yourAge);
-
-//stay consistent when using single or double quotes
-console.log("Hello my name is " + "Grace");
-console.log("Hello it's me Grace");
-
 const name = "Grace";
-const age = 18;
+const color = "blue";
 
-console.log(`Hello it's me and my name is ${name}`);
+function upperCase() {} //function format, parenthesis = parameter -> input value/variable inside it
 
-console.log(
-  "Hello it's me " + name + " and my age is " + age + " and I am getting old"
-);
+function logger() {
+  // const name = "Grace"; //won't be available outside function
+  console.log("Party Time");
+  console.log("Party Time");
+  console.log("Party Time");
+  console.log("Party Time");
+} //variables added inside function aren't available outside of the function
 
-console.log(`Hello it's me ${name} and my age is ${age}`);
+logger(); //where you want to execute function
 
-const combined = name + age;
-console.log(combined); //combined = string -> Grace18
-console.log(typeof combined);
-console.log(typeof age);
+console.log(name);
+
+function toUpper(text) {
+  const upperCased = text.toUpperCase();
+  console.log(upperCased);
+}
+
+toUpper(name);
+toUpper(color);
+
+function adder(num1, num2) {
+  console.log(num1 + num2);
+}
+
+adder(5, 10);
+
+//other ways to write functions
+const toUpper = function () {
+  text;
+};
+
+const toUpper = () => {
+  text;
+};
