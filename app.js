c51cac759b73a51c68a279778d949ef41ed09b5e
gracel202 <linlgrace556@gmail.com>
2021-03-08 23:58:07 -0500

    hi

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
-//comments in js
-var life = 100; //number
-life = life - 10; //90
-life = life - 10; //80
-console.log(life);
+//DOM = Document Object Model, created by the browser when a web page is located
+//looks like tree of elements/nodes in graphical form
+//can use js to read or change the DOM
 
-// var name = "Grace"; //string
-// console.log(name);
+//interacting with the DOM
+//-change/remove HTML elements in the DOM or on the pg, change and add css styles to elements
+//-read and change element attributes, create new html elements and insert them into DOM/page
+//-attach event listeners to elements (click, keypress, submit)
 
-console.log("hey");
+//get element by ID
+document.getElementById("page-banner"); //grabbing an element from the document
 
-var checkout = true; //boolean
-var checkout = false; //boolean
+var banner = document.getElementById("page-banner");
+var bookList = document.getElementById("book-list");
 
-var box;
-console.log(box); //undefined
+//get elements by class or tag
+var titles = document.getElementsByClassName("title"); //results in HTMLCollection, titles [0], titles [1]
 
-var box = null; //no value
+var lis = document.getElementsByTagName("li");
+lis[1];
 
-// use const for selecting img
-// const life = 100;
-// can't redfine life
+for (i = 0; i < titles.length; i++) {
+  console.log(titles[i]);
+} //i starts with 0 but will increase by 1 each time until it reaches length of collection
 
-// let life = 100;
-// life = life - 50;
-// console.log(life);
+//turn titles into array
+var titles = document.getElementsByClassName("title");
+console.log(Array.isArray(titles)); //is false since it's not an array
+console.log(Array.isArray(Array.from(titles))); //returns true
 
-const myAge = 18;
-const yourAge = 21;
-console.log(myAge + yourAge);
+Array.from(titles).forEach(function (item) {
+  console.log(item);
+}); //returns as an array
 
-//stay consistent when using single or double quotes
-console.log("Hello my name is " + "Grace");
-console.log("Hello it's me Grace");
+//query selector
+//$("#wrapper"); //grab wrapper element and return it
 
-const name = "Grace";
-const age = 18;
+const wrap = document.querySelector("#wrapper");
+console.log(wrap); //same function as above
 
-console.log(`Hello it's me and my name is ${name}`);
+const wmf = document.querySelector("#book-list li:nth-child(2) .name");
+console.log(wmf); //grabs 2nd book on list
 
-console.log(
-  "Hello it's me " + name + " and my age is " + age + " and I am getting old"
-);
+var books = document.querySelector("#book-list li .name");
+console.log(books); //grabs only 1st book b/c querySelector only returns 1 single element not multiple ones
 
-console.log(`Hello it's me ${name} and my age is ${age}`);
-
-const combined = name + age;
-console.log(combined); //combined = string -> Grace18
-console.log(typeof combined);
-console.log(typeof age);
+books = document.querySelectorAll("#book-list li .name");
+//returns collection of elements, grabs every element with class of name and li in book-list id
+console.log(books);
