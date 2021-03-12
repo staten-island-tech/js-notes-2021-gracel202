db13342b0f6b982634fc9c1809e3bbead8160297
gracel202 <linlgrace556@gmail.com>
2021-03-12 00:09:43 -0500

    hi

--- app.js
+++ app.js
@@ -47,3 +47,31 @@ console.log(books); //grabs only 1st book b/c querySelector only returns 1 singl
 books = document.querySelectorAll("#book-list li .name");
 //returns collection of elements, grabs every element with class of name and li in book-list id
 console.log(books);
+
+//traversing the DOM (part 1)
+const bookLis = document.querySelector("#book-list");
+console.log("the parent node is:", bookList.parentNode);
+console.log("the parent element is:", bookList.parentElement); //most of the time, these 2 are the same, traverse upward through DOM
+
+console.log("the parent element is:", bookList.parentElement.parentElement); //finds the parent element of the book list, then find parent element of that
+
+//traverse downward through DOM
+console.log(bookLis.childNodes);
+console.log(bookLis.children); //gets element children and not all text nodes
+
+//traversing the DOM (part 2)
+//DOM traversal from sibling to sibling
+console.log("book-list next sibling is:", bookLis.nextSibling); //results in text node
+console.log("book-list next element sibling is:", bookLis.nextElementSibling);
+
+console.log("book-list previous sibling is:", bookLis.previousSibling);
+console.log(
+  "book-list previous element sibling is:",
+  bookLis.previousElementSibling
+);
+
+bookLis.previousElementSibling.querySelector("p").innerHTML +=
+  "<br />Too cool for everyone else!";
+//will search inside header for p tag
+//grab reference to book list, use previous element sibling property to get header, using query selector so it's querying everything inside header
+//find P, change inner HTML of that
