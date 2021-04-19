ff4b65748250feca7613af40261ce85db3dbf53b
gracel202 <linlgrace556@gmail.com>
2021-04-19 00:26:56 -0400

    stuff

--- app.js
+++ app.js
@@ -1,50 +1,24 @@
-//comments in js
-var life = 100; //number
-life = life - 10; //90
-life = life - 10; //80
-console.log(life);
+//promises: need to wait for something to happen before you can run rest of code
 
-// var name = "Grace"; //string
-// console.log(name);
+function makePizza() {
+  const pizzaPromise = new Promise(function (resolve, reject) {
+    //when you're ready, you can resolve this promise
+    resolve("pizza");
+    //if something went wrong, we can reject this promise;
+  });
+  return pizzaPromise;
+}
 
-console.log("hey");
+const pizza = makePizza();
+console.log(pizza);
 
-var checkout = true; //boolean
-var checkout = false; //boolean
+function makePizza(toppings) {
+  const pizzaPromise = new Promise(function (resolve, reject) {
+    resolve("Here is your pizza with the toppings ${toppings.join('')}`");
+  });
+  return pizzaPromise;
+}
 
-var box;
-console.log(box); //undefined
-
-var box = null; //no value
-
-// use const for selecting img
-// const life = 100;
-// can't redfine life
-
-// let life = 100;
-// life = life - 50;
-// console.log(life);
-
-const myAge = 18;
-const yourAge = 21;
-console.log(myAge + yourAge);
-
-//stay consistent when using single or double quotes
-console.log("Hello my name is " + "Grace");
-console.log("Hello it's me Grace");
-
-const name = "Grace";
-const age = 18;
-
-console.log(`Hello it's me and my name is ${name}`);
-
-console.log(
-  "Hello it's me " + name + " and my age is " + age + " and I am getting old"
-);
-
-console.log(`Hello it's me ${name} and my age is ${age}`);
-
-const combined = name + age;
-console.log(combined); //combined = string -> Grace18
-console.log(typeof combined);
-console.log(typeof age);
+const pepperoniPromise = makePizza(["pepperoni"]);
+const canadianPromise = makePizza(["pepperoni", "mushrooms", "onions"]);
+console.log(pepperoniPromise, canadianPromise);
